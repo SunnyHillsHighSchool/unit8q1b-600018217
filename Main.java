30cc97497d17898b11e679910a3e9a669e950f0b
Johnathan Mitri <>
2021-04-28 02:39:50 +0000

    fix

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 import java.util.*;
 class Main {
   public static void main(String[] args) {
-    int[][]array = {{1,5,4,5,7,3},{2,4,3,5,7,6},{2,4,3,3,6,0},{9,8,9,9,4,1}};
+    int[][]array = {{1,5,4,5,7,3},
+                    {2,4,3,5,7,6},
+                    {2,4,3,3,6,0},
+                    {9,8,9,9,4,1}};
 for(int [] row:array)
 System.out.println(Arrays.toString(row));
 System.out.println();
@@ -14,7 +17,7 @@ System.out.println(Arrays.toString(row));
 }
 
 //output should be
-//1 5 4 8 7 3
-//1 5 4 8 7 3
+//1 5 4 5 7 3
+//1 5 4 5 7 3
 //2 4 3 3 6 0
 //2 4 3 3 6 0
